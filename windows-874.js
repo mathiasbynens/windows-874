f88b50c5e4bf57c6cf50cebd4dce7fce610f06d6
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:20:52 +0200

    Rebuild library

--- windows-874.js
+++ windows-874.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/windows-874 v1.0.0 by @mathias | MIT license */
+/*! https://mths.be/windows-874 v1.0.1 by @mathias | MIT license */
 ;(function() {
 
 	var object = {};
@@ -107,7 +107,7 @@
 			'tis-620',
 			'windows-874'
 		],
-		version: '1.0.0',
+		version: '1.0.1',
 	};
 
 	module.exports = windows874;
